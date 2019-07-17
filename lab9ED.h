44b897c215d02fb5c05c6f57cf163a0253b1caac
DiegoHDLC <dherreradelacalle@gmail.com>
2019-07-16 23:38:03 -0400

    Subiendo 1 semana después

---
+++ lab9ED.h
@@ -0,0 +1,45 @@
+
+
+typedef struct _tablaHash{
+  char palabra[20];
+  struct _tablaHash *siguiente;
+}THASH[27];
+
+
+void creatablahash(char *nombre, int tamano){
+  tabla = creaTabla();
+  FILE *p;
+  char c, palabra[20];
+  int contador = 0;
+  palabra = creaArreglo(palabra);
+  p = fopen(nombre, "r");
+  while((c = fgetc(p)) != EOF){
+    if(c == '\n'){
+      contador = 0;
+      pos = convertirLetras(palabra[0]);
+      THASH[pos] =
+
+    }
+    else{
+    palabra[contador] = c;
+    contador++;
+    }
+  }
+}
+
+int convertirLetras(char letra){
+  char str[2] = {letra};
+  int num = strtol(str, NULL, 36) - 10;
+  return num;
+}
+
+THASH * creaTabla(){
+
+}
+
+char *vaciarArreglo(char *palabra){
+  for(int i; i < 20; i++){
+    palabra[i] = '\0';
+  }
+  return palabra;
+}
